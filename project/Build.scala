c05958d7fc496eae2e61db158afe5bb83d56aa11
Jeremy Hare <jeremy.hare@gmail.com>
2013-08-26 21:20:17 +1000

    version  0.0.3

--- project/Build.scala
+++ project/Build.scala
@@ -5,7 +5,7 @@ import play.Project._
 object ApplicationBuild extends Build {
 
   val appName         = "play-stretchy"
-  val appVersion      = "0.0.3-SNAPSHOT"
+  val appVersion      = "0.0.3"
 
 
   val appDependencies = Seq(
